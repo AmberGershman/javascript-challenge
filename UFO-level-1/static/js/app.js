5cef0a42e7da37a902a461d351f929ca7208edae
Amber Gershman <67204846+AmberGershman@users.noreply.github.com>
2020-11-18 19:52:15 -0500

    Easily able to write the table and was the CSS already supposed to be in there? It feels easy! I say that but the filter function is turning out to be frustrating. Rerendering the table with the input date feels like it should be easier. FROWN

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,12 +4,39 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 //assign values for each cell with forEach loop
-tableData.forEach(function(sighting){
-    var row= tbody.append("tr")
+tableData.forEach(function (instance) {
+    var row = tbody.append("tr")
     //unpack the contents
-    Object.entries(sighting).forEach(function([key,value]){
+    Object.entries(instance).forEach(([key, value]) => {
         // console.log(value)
         var cell = row.append("td")
         cell.text(value)
     })
 });
+
+var search = d3.select("#filter-btn");
+
+function filterDate() {
+    // d3.event.preventDefault();
+    var date = d3.select("#datetime");
+    var datevalue = date.property("value");
+    alert(`Careful, they don't want you to see the UFO sightings on ${datevalue}`);
+
+    var filterDate = data.filter(sighting => sighting.datetime === datevalue);
+
+    console.log(filterDate);
+
+    for (var i; i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+};
+
+search.on("click");
